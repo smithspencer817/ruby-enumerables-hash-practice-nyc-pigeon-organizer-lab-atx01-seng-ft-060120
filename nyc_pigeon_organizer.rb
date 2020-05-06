f2ebc179278f4ff7c32ad67f88302d3d2d983604
Spencer Smith <smithspencer817@gmail.com>
2020-05-06 16:19:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,40 +1,13 @@
-def nyc_pigeon_organizer (data)
-  final = {}
-
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
-      array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
-      end 
-    end 
-  end 
-  x = final.keys
-  data[:color].each do |bird_color, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:color] << bird_color.to_s
-        end 
-      end 
-    end 
-  end 
-  data[:gender].each do |gender, type|
-    type.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:gender] << gender.to_s
-        end 
-      end 
-    end 
-  end 
-  data[:lives].each do |location, name|
-    name.each do |bird_name|
-      x.each do |item|
-        if bird_name === item
-          final[item][:lives] << location
-        end 
-      end 
-    end 
-  end 
-  return final 
-end 
+def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+  data.each do |trait, attributes|
+    attributes.each do |details, names|
+      names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][trait] ||= []
+        pigeon_list[name][trait] << details.to_s
+      end
+    end
+  end
+  return pigeon_list
+end
